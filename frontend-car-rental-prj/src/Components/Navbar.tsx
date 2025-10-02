94a223d574a3b69656e9c2f9575fb0e4966586c0
Casper Banemann <casperbanemann@gmail.com>
2025-10-02 14:11:12 +0200

    Refactor Navbar and main components: remove ColorModeSwitch and update ChakraProvider configuration

--- frontend-car-rental-prj/src/Components/Navbar.tsx
+++ frontend-car-rental-prj/src/Components/Navbar.tsx
@@ -1,13 +1,11 @@
 import { HStack, Image } from "@chakra-ui/react";
-
 import logo from "../assets/logo.webp";
-import { ColorModeSwitch } from "./ColorModeSwitch";
+
 
 export const NavBar = () => {
   return (
     <HStack justifyContent={"space-between"} padding={4}>
       <Image src={logo} boxSize="60px" />
-      <ColorModeSwitch />
     </HStack>
   );
 };
--- frontend-car-rental-prj/src/main.tsx
+++ frontend-car-rental-prj/src/main.tsx
@@ -1,16 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import theme from "./theme";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
+import './index.css'
+import App from './App.tsx'
 
-
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ChakraProvider theme={theme}>
-    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <ChakraProvider value={defaultSystem}>
       <App />
     </ChakraProvider>
-  </StrictMode>
-);
+  </StrictMode>,
+)
