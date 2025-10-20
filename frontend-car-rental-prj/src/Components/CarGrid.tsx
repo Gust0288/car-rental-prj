2b968e7aa3a7efa280402c73ff8b3f9f2285b335
Gustav <gubo0001@stud.kea.dk>
2025-10-20 09:26:54 +0200

    package update and fallback loading

--- frontend-car-rental-prj/src/Components/CarGrid.tsx
+++ frontend-car-rental-prj/src/Components/CarGrid.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import { CarCard } from "./CarCard";
 
 interface Car {
@@ -14,15 +14,15 @@ interface CarGridProps {
 }
 
 export const CarGrid = ({ cars, onCarClick }: CarGridProps) => {
+  if (!cars || !Array.isArray(cars)) {
+    return <Text>...Loading</Text>;
+  }
   return (
-    <SimpleGrid 
-      columns={{ base: 1, sm: 2, md: 3, lg: 4 }} 
-      gap={6}
-      w="100%"
-    >
+    <SimpleGrid columns={{ base: 1, sm: 2, md: 3, lg: 4 }} gap={6} w="100%">
       {cars.map((car) => (
         <CarCard
           key={car.id}
+          id={car.id}
           make={car.make}
           model={car.model}
           imageUrl={car.img_path}
